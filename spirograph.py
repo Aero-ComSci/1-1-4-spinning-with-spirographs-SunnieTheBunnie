6c499a15578596b6eb9c61a6958f40eb77dc65c7
SunnieTheBunnie <148286895+SunnieTheBunnie@users.noreply.github.com>
2024-09-11 14:25:07 -0700

    Update spirograph.py
    Changed my code so that it will add fill colors

--- spirograph.py
+++ spirograph.py
@@ -1,20 +1,25 @@
 import turtle as trtl
+import random
 
 t = trtl.Turtle()
 
 t.speed(20)
 
-x = 10
-move_x = 40
+x = 300
+move_x = 25
+colors = ["red", "blue", "orange", "yellow", "green", "purple"]
 
 for i in range(9):
-    x = x + move_x
+    x = x - move_x
     t.penup()
+    t.fillcolor(random.choice(colors))
+    t.begin_fill()
     t.goto(-x/2,x/2)
     t.pendown()
     for _ in range(4):
-        t.forward(x + move_x)
+        t.forward(x - move_x)
         t.right(90)
+    t.end_fill()
 
 wn = trtl.Screen()
 wn.mainloop()
