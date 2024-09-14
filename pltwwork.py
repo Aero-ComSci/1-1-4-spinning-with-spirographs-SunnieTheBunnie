490c661e94a6c4fe92be77bf53c7828d327e0c22
SunnieTheBunnie <148286895+SunnieTheBunnie@users.noreply.github.com>
2024-09-13 19:10:12 -0700

    Update pltwwork.py
    completed steps 17-19, functions properly

--- pltwwork.py
+++ pltwwork.py
@@ -5,41 +5,47 @@ painter.penup()
 painter.goto(-200, 0)
 painter.pendown()
 
-x = -200
-y = 0
-move_x = 1
-move_y = 1
-while (x < 100):
-
-  while (y < 100):
-    x = x + move_x
-    y = y + move_y
-    painter.goto(x,y)
-  move_y = -1
-  
-  while (y > 0):
-    x = x + move_x
-    y = y + move_y
-    painter.goto(x,y)
+while True #used for step 19
+  x = -200
+  y = 0
+  move_x = 1
   move_y = 1
-
-x = 0
-y = -200
-move_x = -1
-move_y = -1
-while (x < 100):
-
-  while (y > 100):
-    x = x + move_x
-    y = y + move_y
-    painter.goto(x,y)
-  move_y = 1
+  while (x < 200): #changed 0 to 200 for step 17
   
-  while (y < 0):
-    x = x + move_x
-    y = y + move_y
-    painter.goto(x,y)
+    while (y < 100):
+      x = x + move_x
+      y = y + move_y
+      painter.goto(x,y)
+    move_y = -1
+    
+    while (y > 0):
+      x = x + move_x
+      y = y + move_y
+      painter.goto(x,y)
+    move_y = 1
+  
+  painter.penup()
+  painter.goto(200,0)
+  painter.pendown()
+  
+  #start of step 18
+  x = 200
+  y = 0
+  move_x = -1
   move_y = -1
+  while (x > -200):
+  
+    while (y > -100):
+      x = x + move_x
+      y = y + move_y
+      painter.goto(x,y)
+    move_y = 1
+    
+    while (y < 0):
+      x = x + move_x
+      y = y + move_y
+      painter.goto(x,y)
+    move_y = -1
 
 
 wn = trtl.Screen()
